391757435a372a5ce3eb1f482b882f9f68dc4f1f
zack pulaski <zackpulaski@unknown5ce0c58e8154.attlocal.net>
2024-11-03 15:09:02 -0500

    disable buffer highlight

--- init.lua
+++ init.lua
@@ -434,7 +434,7 @@ require("lazy").setup({
 		"olimorris/onedarkpro.nvim",
 		priority = 1000,
 		config = function()
-			require("onedarkpro").setup({ options = { highlight_inactive_windows = true } })
+			require("onedarkpro").setup({ options = { highlight_inactive_windows = false } })
 		end,
 		init = function()
 			vim.cmd.colorscheme("onedark")
